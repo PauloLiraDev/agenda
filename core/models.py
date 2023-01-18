16fccaef1e792769be7dffd4ce2f6fa4220395d3
Paulo Lira <paulocesar_cl@hotmail.com>
2023-01-17 23:31:20 -0300

    Novas funcionalidades

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(verbose_name='Data de Criação', auto_now=True)  # Insere a data atual para o banco de dados.
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+    def __str__(self):
+        return self.titulo
+
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
+
+# def index(request):
+#     return redirect('/agenda/')
+
+
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Credenciais (Usuário e/ou Senha) inválidas.")
+    return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario).order_by('data_evento')
+    if not evento.exists():
+        dados = {'mensagem': f'{usuario}, não há agendamentos disponíveis para você.'}
+        return render(request, 'agenda.html', dados)
+    else:
+        dados = {'mensagem': f'Olá, {usuario}. Estes são seus agendamentos:', 'eventos': evento, 'usuario': usuario}
+        return render(request, 'agenda.html', dados)
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
