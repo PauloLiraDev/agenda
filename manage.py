93a569c6c778bc2a22c1b211ba090eccd973c45a
Paulo Lira <paulocesar_cl@hotmail.com>
2023-01-23 18:17:43 -0300

    Alterações no Front-End
    Implementação de fonte, posicionamentos e reajustes de cores.

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!C:\virtualenvs\dev_django\Scripts\python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -20,3 +20,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
